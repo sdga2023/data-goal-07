b95f1ffacd7776cabba0ecd55e7751b112dce1fd
Javier <javoparada@gmail.com>
2023-09-07 15:48:16 -0400

    describe more steps

--- code/sdg07-dataprep.R
+++ code/sdg07-dataprep.R
@@ -11,7 +11,7 @@ countries <- wb_countries()
 # Sheet "accessperc" for Figure 1 --------------------------------------
 
 # Download access with API
-access.electricity <- wb_data(indicator = c("EG.ELC.ACCS.ZS"),country = "countries_only")
+access.electricity <- wb_data(indicator = c("EG.ELC.ACCS.ZS"), country = "countries_only")
 access.electricity.world <- wb_data(indicator = c("EG.ELC.ACCS.ZS"), country = "WLD")
 access.electricity.regions <- wb_data(indicator = c("EG.ELC.ACCS.ZS"), country = "regions_only")
 access.all <-rbind(access.electricity, access.electricity.regions, access.electricity.world)
@@ -23,32 +23,38 @@ access.all.ok <- select(access.all, -iso2c, -country) %>%
   mutate(value = round(value, 1)) %>%
   #filter(date %% 2 == 0) %>%
   select(iso3c, date, value)
+# Export
 write.csv(access.all.ok, "../output/accessperc.csv", row.names = FALSE)
 
 # Sheet "accessabsolute" for Figure 2 --------------------------------------
 
-# Access
+# Download access with API
 access.rururb <- wb_data(indicator = c('EG.ELC.ACCS.RU.ZS', "EG.ELC.ACCS.UR.ZS"), country = "regions_only")
 access.rururb.ok <- select(access.rururb, iso3c, date, EG.ELC.ACCS.RU.ZS, EG.ELC.ACCS.UR.ZS) %>%
   filter(date > 1999 & date <= 2021)
-# Population
+# Download population with API
 pop.rururb <- wb_data(indicator = c('SP.RUR.TOTL', 'SP.URB.TOTL'), country = "regions_only")
 pop.rururb.ok <- select(pop.rururb, iso3c, date, SP.RUR.TOTL, SP.URB.TOTL) %>%
   filter(date > 1999 & date <= 2021)
+# Calculate population without electricity by urban and rural
 access.pop <- left_join(access.rururb.ok, pop.rururb.ok, by = c("iso3c", "date")) %>%
   mutate(pop_without_urb = round(SP.URB.TOTL * (100 - EG.ELC.ACCS.UR.ZS)/100)) %>%
   mutate(pop_without_rur = round(SP.RUR.TOTL * (100 - EG.ELC.ACCS.RU.ZS)/100)) %>%
   select(iso3c, date, pop_without_urb, pop_without_rur)
-## Group Europe and central asia and North America together
-access.pop.def <- mutate(access.pop, iso3c = if_else(iso3c %in% c("NAC", "ECS"), "RESTOFWORLD", iso3c)) %>%
+## Group Europe and Central Asia and North America together
+access.pop.def <- mutate(access.pop, 
+                         iso3c = if_else(iso3c %in% c("NAC", "ECS"), "RESTOFWORLD", iso3c)
+                         ) %>%
   group_by(iso3c, date) %>%
   summarise(pop_without_urb = sum(pop_without_urb), pop_without_rur = sum(pop_without_rur))
+# Export
 write.csv(access.pop.def, "../output/accessabsolute.csv", row.names = FALSE)
 
 # Tiers
 
 # Sheet "tiers" for Figure 3 --------------------------------------
 
+# Import data
 tiers <- read_excel("../input/Figure3.xlsx")
 tiers <- select(tiers, -Num, -Country, -Year)
 colnames(tiers) <- c("iso3c", "rururb", "tier0", "tier1", "tier2", "tier3", "tier4", "tier5")
@@ -67,20 +73,21 @@ write.csv(tiers.ok, "../output/tiers.csv", row.names = FALSE)
 
 # Figure 4. Access vs emissions
 
-# Emissions
+# Import Emissions data downloaded from CAIT
 emis.raw <- read.csv('../input/historical_emissions_ciat_updated.csv', na.strings = "FALSE")
-# Access
+# Download access (regions+world) with API
 access.electricity.regions <- wb_data(indicator = c("EG.ELC.ACCS.ZS"), country = "regions_only")
 access.electricity.world <- wb_data(indicator = c("EG.ELC.ACCS.ZS"), country = "world")
 access.electricity.ok <- rbind(access.electricity.regions, access.electricity.world) %>%
   select(iso3c, access = EG.ELC.ACCS.ZS, year = date) %>%
   filter(year > 1999, year < 2021)
-# Population
+# Download population (regions+world) with API
 pop.regions <- wb_data(indicator = c("SP.POP.TOTL"), country = "regions_only")
 pop.world <- wb_data(indicator = c("SP.POP.TOTL"), country = "world")
 pop.regions.ok <- rbind(pop.regions, pop.world) %>%
   select(iso3c, pop = SP.POP.TOTL, year = date)
 
+# Match countries to their region
 country.regions <- select(countries, iso3c, region_iso3c)
 
 emis <- select(emis.raw, -Data.source, -Sector, -Gas, -Unit) %>%
@@ -93,6 +100,7 @@ emis <- select(emis.raw, -Data.source, -Sector, -Gas, -Unit) %>%
   filter(!is.na(region_iso3c)) %>% 
   filter(!is.na(emissions))
 
+# Aggregate emissions by region
 emis.regions <- group_by(emis, region_iso3c, year) %>%
   summarise(emissions = sum(emissions)) %>%
   filter(year > 1999) %>%
@@ -103,6 +111,7 @@ access.pop.emis <- left_join(emis.regions, pop.regions.ok,  access.electricity.o
   left_join(access.electricity.ok, by = c("iso3c", "year")) %>%
   mutate(emissions_percap = emissions*1000000/pop)
 
+# Export
 access.pop.emis.ok <- select(access.pop.emis, iso3c, year, access, emissions_percap) %>%
   mutate(access = round(access, 2)) %>%
   mutate(emissions_percap = round(emissions_percap, 2))
