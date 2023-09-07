37e503262fb6bbe01826897fb819bb72675539d9
Javier <javoparada@gmail.com>
2023-09-07 15:02:09 -0400

    describe steps

--- code/sdg07-dataprep.R
+++ code/sdg07-dataprep.R
@@ -7,6 +7,9 @@ library(countrycode)
 countries <- wb_countries()
 
 # Access shares
+
+# Sheet "accessperc" for Figure 1 --------------------------------------
+
 access.electricity <- wb_data(indicator = c("EG.ELC.ACCS.ZS"))
 access.electricity.world <- wb_data(indicator = c("EG.ELC.ACCS.ZS"), country = "WLD")
 access.electricity.regions <- wb_data(indicator = c("EG.ELC.ACCS.ZS"), country = "regions_only")
@@ -20,9 +23,13 @@ access.all.ok <- select(access.all, -iso2c, -country) %>%
   select(iso3c, date, value)
 write.csv(access.all.ok, "../output/accessperc.csv", row.names = FALSE)
 
+# Sheet "accessabsolute" for Figure 2 --------------------------------------
+
+# Access
 access.rururb <- wb_data(indicator = c('EG.ELC.ACCS.RU.ZS', "EG.ELC.ACCS.UR.ZS"), country = "regions_only")
 access.rururb.ok <- select(access.rururb, iso3c, date, EG.ELC.ACCS.RU.ZS, EG.ELC.ACCS.UR.ZS) %>%
   filter(date > 1999 & date <= 2021)
+# Population
 pop.rururb <- wb_data(indicator = c('SP.RUR.TOTL', 'SP.URB.TOTL'), country = "regions_only")
 pop.rururb.ok <- select(pop.rururb, iso3c, date, SP.RUR.TOTL, SP.URB.TOTL) %>%
   filter(date > 1999 & date <= 2021)
@@ -37,6 +44,9 @@ access.pop.def <- mutate(access.pop, iso3c = if_else(iso3c %in% c("NAC", "ECS"),
 write.csv(access.pop.def, "../output/accessabsolute.csv", row.names = FALSE)
 
 # Tiers
+
+# Sheet "tiers" for Figure 3 --------------------------------------
+
 tiers <- read_excel("../input/Figure3.xlsx")
 tiers <- select(tiers, -Num, -Country, -Year)
 colnames(tiers) <- c("iso3c", "rururb", "tier0", "tier1", "tier2", "tier3", "tier4", "tier5")
@@ -51,15 +61,19 @@ pop.shares.ok <- filter(pop.shares, date == 2020) %>%
 tiers.ok <- left_join(tiers.long, pop.shares.ok, by = "iso3c")
 write.csv(tiers.ok, "../output/tiers.csv", row.names = FALSE)
 
+# Sheet "access_emissions" for Figure 4 --------------------------------------
+
 # Figure 4. Access vs emissions
+
+# Emissions
 emis.raw <- read.csv('../input/historical_emissions_ciat_updated.csv', na.strings = "FALSE")
+# Access
 access.electricity.regions <- wb_data(indicator = c("EG.ELC.ACCS.ZS"), country = "regions_only")
 access.electricity.world <- wb_data(indicator = c("EG.ELC.ACCS.ZS"), country = "world")
-
 access.electricity.ok <- rbind(access.electricity.regions, access.electricity.world) %>%
   select(iso3c, access = EG.ELC.ACCS.ZS, year = date) %>%
   filter(year > 1999, year < 2021)
-
+# Population
 pop.regions <- wb_data(indicator = c("SP.POP.TOTL"), country = "regions_only")
 pop.world <- wb_data(indicator = c("SP.POP.TOTL"), country = "world")
 pop.regions.ok <- rbind(pop.regions, pop.world) %>%
@@ -68,7 +82,7 @@ pop.regions.ok <- rbind(pop.regions, pop.world) %>%
 country.regions <- select(countries, iso3c, region_iso3c)
 
 emis <- select(emis.raw, -Data.source, -Sector, -Gas, -Unit) %>%
-  pivot_longer(cols = 3:33, names_to = "year", values_to = "emissions") %>%
+  pivot_longer(cols = starts_with("X"), names_to = "year", values_to = "emissions") %>%
   mutate(year = as.numeric(gsub("X", "", year))) %>%
   mutate(iso3c = countrycode(Country, origin = "country.name", destination = "iso3c")) %>%
   mutate(iso3c = if_else(Country == "World", "WLD", iso3c)) %>%
@@ -82,6 +96,7 @@ emis.regions <- group_by(emis, region_iso3c, year) %>%
   filter(year > 1999) %>%
   rename(iso3c = region_iso3c)
 
+# Calculate emissions per capita
 access.pop.emis <- left_join(emis.regions, pop.regions.ok,  access.electricity.ok, by = c("iso3c", "year")) %>%
   left_join(access.electricity.ok, by = c("iso3c", "year")) %>%
   mutate(emissions_percap = emissions*1000000/pop)
@@ -92,10 +107,14 @@ access.pop.emis.ok <- select(access.pop.emis, iso3c, year, access, emissions_per
 write.csv(access.pop.emis.ok, "../output/access_emissions.csv", row.names = FALSE)
 
 
+# Sheet "electricitysources" for Figure 5 --------------------------------------
+
 # Figure 5: Sources of electricity
+# Import
 offgrid <- read.csv("../input/offgrid.csv", na.strings = "..")
 ongrid <- read.csv("../input/ongrid.csv", na.strings = "..")
 sources.all <- rbind(offgrid, ongrid)
+# Recode energy sources
 sources.all.ok <- mutate(sources.all, Country.area = if_else(Country.area == "T<fc>rkiye", "Turkey", Country.area)) %>%
   mutate(iso3c = countrycode(Country.area, origin="country.name", destination="iso3c")) %>%
   filter(!is.na(Electricity.generation..GWh.)) %>%
